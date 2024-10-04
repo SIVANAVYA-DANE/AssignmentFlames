d159d6b6958e45f5033e5fc190c5c3795a0990bc
SIVANAVYA-DANE <155898682+SIVANAVYA-DANE@users.noreply.github.com>
2024-10-04 11:02:54 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,1 +1,27 @@
 
+import {useState, useContext} from 'react'
+import SearchBar from '../components/SearchBar'
+import BookList from '../components/BookList'
+import {LibraryContext} from '../context/LibraryContext'
+
+const HomePage = () => {
+  const {books} = useContext(LibraryContext)
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const filteredBooks = books.filter(
+    book =>
+      book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      book.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      book.genre.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
+
+  return (
+    <div className="homepage">
+      <h1>Book Library</h1>
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <BookList books={filteredBooks} />
+    </div>
+  )
+}
+
+export default HomePage
