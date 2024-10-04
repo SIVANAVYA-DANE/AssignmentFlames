a3b1d3adb11a61980692d4ed480857eae2ec3235
SIVANAVYA-DANE <155898682+SIVANAVYA-DANE@users.noreply.github.com>
2024-10-04 10:57:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BookDetailsPage from './pages/BookDetailsPage'
+import MyLibraryPage from './pages/MyLibraryPage'
+import LibraryProvider from './context/LibraryContext'
+import './styles/styles.css'
+
+const App = () => (
+  <LibraryProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/book/:id" element={<BookDetailsPage />} />
+        <Route path="/mylibrary" element={<MyLibraryPage />} />
+      </Routes>
+    </Router>
+  </LibraryProvider>
+)
+
+export default App
