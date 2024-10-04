06c97ab706249c0385dadc779e32f8c4add250d1
SIVANAVYA-DANE <155898682+SIVANAVYA-DANE@users.noreply.github.com>
2024-10-04 11:06:03 +0530

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,1 +1,16 @@
 
+import BookCard from './BookCard'
+
+const BookList = ({books, removeBook}) => (
+  <div className="book-list">
+    {books.length > 0 ? (
+      books.map(book => (
+        <BookCard key={book.id} book={book} removeBook={removeBook} />
+      ))
+    ) : (
+      <p>No books found.</p>
+    )}
+  </div>
+)
+
+export default BookList
